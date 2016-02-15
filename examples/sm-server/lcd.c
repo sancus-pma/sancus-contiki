397b9667552c21635f2cc476654e04e0d73b57e4
Job Noorman <job.noorman@cs.kuleuven.be>
2016-02-15 17:16:19 +0100

    Add LCD driver symbols to the symtab.

--- examples/sm-server/lcd.c
+++ examples/sm-server/lcd.c
@@ -3,11 +3,23 @@
 #include "lcd_mmio.h"
 #include "lcd_driver.h"
 
+#include <sancus_support/global_symtab.h>
+
+static void init_symbols()
+{
+#define SYM(name) extern char name; add_global_symbol(#name, &name, NULL)
+    SYM(__sm_lcd_driver_entry_lcd_driver_write_idx);
+    SYM(__sm_lcd_driver_entry_lcd_driver_acquire_idx);
+    SYM(__sm_lcd_driver_entry);
+#undef SYM
+}
+
 void lcd_init()
 {
     sancus_enable(&lcd_mmio);
     sancus_enable(&lcd_driver);
     lcd_driver_init();
+    init_symbols();
 }
 
 void lcd_clear()
